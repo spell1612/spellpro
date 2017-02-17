8f89e70635625756b7c925235f66eef0df28d8b3
spell1612 <spell1612@gmail.com>
2017-02-17 14:00:25 +0530

    john cena

---
+++ app/Http/Controllers/iamno1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class iamno1 extends Controller
+{
+    public function myfun($name){
+            echo "dove".$name;
+    }
+}

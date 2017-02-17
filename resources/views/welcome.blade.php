c9ee023b87eff71d8aefb75d6ea02f2a9b3bce1a
spell1612 <spell1612@gmail.com>
2017-02-17 11:26:20 +0530

    cancer meme edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>YEAAAA</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    YEAAA <BR>BOIIII
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">iTS</a>
+                    <a href="https://laracasts.com">DAT BOI</a>
+                    <a href="https://laravel-news.com">OH </a>
+                    <a href="https://forge.laravel.com">SHIT </a>
+                    <a href="https://github.com/laravel/laravel">WHADDUP</a>
                 </div>
             </div>
         </div>
